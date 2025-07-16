dcf2cf28e666f313df781e10d504f0c8a6ad825d
Hisham Mohamed <heshamkoptan@gmail.com>
2025-07-16 04:24:49 +0300

    first commit

---
+++ lib/form_inputs.dart
@@ -0,0 +1,9 @@
+library form_inputs;
+
+export 'form_inputs/email_input.dart';
+export 'form_inputs/password_input.dart';
+export 'form_inputs/confirm_password_input.dart';
+export 'form_inputs/phone_number_input.dart';
+export 'form_inputs/file_formz_input.dart';
+export 'form_inputs/generic_formz_input.dart';
+export 'form_inputs/generic_checkbox_input.dart';
